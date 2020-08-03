f863bd6de45edc56bd36060e3c9e3936de42d164
analitik9 <analitik9@gmail.com>
2020-08-03 12:14:09 +0300

    Initial Commit

---
+++ TheGameSet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheGameSet
+//
+//  Created by Denis Evdokimov on 03.08.2020.
+//  Copyright © 2020 Denis Evdokimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
